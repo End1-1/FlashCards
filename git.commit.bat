16064531339cddaa890ca0810f090d376e514230
user <end1_1@yahoo.com>
2021-03-09 17:28:45 +0400

    L

--- git.commit.bat
+++ git.commit.bat
@@ -1,4 +1,4 @@
-C:\MySQL\bin\mysqldump -uroot -posyolia flashcards > c:\Projects\FlashCards\db.FlashCards.bak
+C:\Soft\MySQL\bin\mysqldump -uroot -posyolia flashcards > c:\Projects\FlashCards\db.FlashCards.bak
 git add .
 git commit
 git push
